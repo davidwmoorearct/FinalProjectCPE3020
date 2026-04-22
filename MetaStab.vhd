07dbb45ee199fa4bf201f52b1f5a9395dd3cc36c
davidwmoorearct <158094974+davidwmoorearct@users.noreply.github.com>
2026-04-21 22:16:54 -0400

    HOLY FUCK IT'S DONE

--- MetaStab.vhd
+++ MetaStab.vhd
@@ -1,35 +1,13 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 04/21/2026 04:52:05 PM
--- Design Name: 
--- Module Name: MetaStab - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
+------------------------------------------------------------------------------------------
+-- Lab 04
+-- David W. Moore & Nate Powell
+--
+-- Very simple two flip flop metastabilizer, with a monoprocedure to do all the meta-
+-- stabilization at once for the project.
+------------------------------------------------------------------------------------------
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 package MetaStab is
 procedure META_STABILITY 
